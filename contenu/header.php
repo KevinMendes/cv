289397048c0db563d7f2c4e0b5932722767f8d72
KevinMendes <kevin.mendes35@gmail.com>
2019-10-29 18:46:36 +0100

    Jumbotron techno à finir

--- contenu/header.php
+++ contenu/header.php
@@ -11,8 +11,8 @@
 <header>
     <nav class="navbar navbar-expand bg-danger">
         <div class="navbar-collapse collapse">
-            <a class="navbar-brand mb-0 h1 nav-item mr-auto text-light" href="#">KEVIN</a>
-            <ul class="navbar-nav ">
+            <a class="navbar-brand mb-0 h1 nav-item mr-auto text-light font-weight-bold" href="#">KEVIN</a>
+            <ul class="navbar-nav text-uppercase">
                 <li class="nav-item active ml-auto"><a class="nav-link text-light" href="#project">Mes projets</a></li>
                 <li class="nav-item ml-auto"><a class="nav-link text-light" href="#technos">Mes technos</a></li>
                 <li class="nav-item ml-auto"><a class="nav-link text-light" href="#who">Qui suis-je ?</a></li>
--- contenu/hire.php
+++ contenu/hire.php
@@ -1,32 +1,32 @@
 <div class="bg-danger pt-2 pb-2" style="background: linear-gradient(-315deg,#333,#bbb);">
-<section id="hire" class="hire m-5 p-2 rounded mx-auto text-light" style="width: 600px;">
-   <h2>Contactez moi</h2>
-        <form action="" method="get" class="m-2 recruts">
-            <div class="row">
-                <div class="col">
-                    <label for="name">Entrez votre nom: </label>
-                    <input type="text" class="form-control" name="name" id="name" required>
+    <section id="hire" class="hire m-5 p-2 rounded mx-auto text-light" style="width: 600px;">
+        <h2>Contactez moi</h2>
+            <form action="" method="get" class="m-2 recruts">
+                <div class="row">
+                    <div class="col">
+                        <label for="name">Entrez votre nom: </label>
+                        <input type="text" class="form-control" name="name" id="name" required>
+                    </div>
+                    <div class="col">
+                        <label for="surname">Entrez votre prénom: </label>
+                        <input type="text" class="form-control" name="surname" id="surname" required>
+                    </div>
                 </div>
-                <div class="col">
-                    <label for="surname">Entrez votre prénom: </label>
-                    <input type="text" class="form-control" name="surname" id="surname" required>
+                <div class="form-group mt-2">
+                    <label for="email">Entrez votre email: </label>
+                    <input type="email" name="email" id="email" class="form-control" place-holder="Entre votre adresse email" required>
                 </div>
-            </div>
-            <div class="form-group mt-2">
-                <label for="email">Entrez votre email: </label>
-                <input type="email" name="email" id="email" class="form-control" place-holder="Entre votre adresse email" required>
-            </div>
-            <div class="form-group">
-                <label for="message">Entrez votre message :</label>
-                <textarea class="form-control" id="message" name="message" place-holder="Votre message"rows="10" cols="20">
-                
-                </textarea>
-            </div>
-            <div class="recruts">
-                <input type="submit" class="btn btn-light" value="Envoyer">
-            </div>
-        </form>
-<div>
+                <div class="form-group">
+                    <label for="message">Entrez votre message :</label>
+                    <textarea class="form-control" id="message" name="message" place-holder="Votre message"rows="10" cols="20">
+                    
+                    </textarea>
+                </div>
+                <div class="recruts">
+                    <input type="submit" class="btn btn-light" value="Envoyer">
+                </div>
+            </form>
+    <div>
 </section>
 
 <?php //  background: linear-gradient(315deg,#E91E63,#5D02FF); ?>
