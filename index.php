013a099c0100b860bc425a5c535b62a0b9269220
KevinMendes <kevin.mendes35@gmail.com>
2019-10-29 12:10:51 +0100

    skelette html

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<header>
+    <nav>
+        <a href="">Mes projets</a>
+        <a href="">Qui suis-je ?</a>
+        <a href="">Recrutez-moi</a>
+    </nav>
+</header>
+<body>
+    
+</body>
+</html>
